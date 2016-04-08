4159ea703824668f47947857014a8bbe463f501c
Kurtaclan <petersontravis@live.com>
2016-04-07 22:42:27 -0700

    Making a bunch of little changes
    Making a bunch of little changes

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLogic : MonoBehaviour
+{
+    public GameObject BaseTile;
+    public GameObject Player;
+    public float PlayerRadius { get; set; }
+    private GameObject currentTile { get; set; }
+    private GameObject nextTile { get; set; }
+
+    void Start()
+    {
+        CreateMap(9, new Vector3(0, 0, 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+        CheckBoundary(Player.transform.position);
+    }
+
+    public void CheckBoundary(Vector3 curPos)
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(curPos, PlayerRadius, 8);
+
+        foreach (var collider in hitColliders)
+        {
+            if (collider.GetComponent<Tile>() != null)
+            {
+                nextTile = collider.gameObject;
+                break;
+            }
+        }
+    }
+
+    public void CreateMap(int tileCount, Vector3 startPosition)
+    {
+        currentTile = (GameObject)Instantiate(BaseTile, startPosition, Quaternion.Euler(0, 0, 0));
+        Instantiate(Player, startPosition, Quaternion.Euler(0, 0, 0));
+        Instantiate(BaseTile, new Vector3(currentTile.transform.localScale.x, 0, currentTile.transform.localScale.z), Quaternion.Euler(0, 0, 0));
+        Instantiate(BaseTile, new Vector3(currentTile.transform.localScale.x, 0, 0), Quaternion.Euler(0, 0, 0));
+        Instantiate(BaseTile, new Vector3(-currentTile.transform.localScale.x, 0, 0), Quaternion.Euler(0, 0, 0));
+        Instantiate(BaseTile, new Vector3(0, 0, currentTile.transform.localScale.z), Quaternion.Euler(0, 0, 0));
+        Instantiate(BaseTile, new Vector3(0, 0, -currentTile.transform.localScale.z), Quaternion.Euler(0, 0, 0));
+        Instantiate(BaseTile, new Vector3(-currentTile.transform.localScale.x, 0, currentTile.transform.localScale.z), Quaternion.Euler(0, 0, 0));
+        Instantiate(BaseTile, new Vector3(currentTile.transform.localScale.x, 0, -currentTile.transform.localScale.z), Quaternion.Euler(0, 0, 0));
+        Instantiate(BaseTile, new Vector3(-currentTile.transform.localScale.x, 0, -currentTile.transform.localScale.z), Quaternion.Euler(0, 0, 0));
+    }
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour
+{
+    public Color CurColor;
+    void Start()
+    {
+        GetComponent<MeshRenderer>().material.SetColor("_Color", new Color(Random.value, Random.value, Random.value));
+        CurColor = GetComponent<MeshRenderer>().material.GetColor("_Color");
+    }
+
+    void Update()
+    {
+        if (GetComponent<MeshRenderer>().material.GetColor("_Color") != CurColor)
+            GetComponent<MeshRenderer>().material.SetColor("_Color", CurColor);
+    }
+}
