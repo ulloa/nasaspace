30a8c5a88f2bb705e49ec1f60c9cedb1c0b87683
Kurtaclan <petersontravis@live.com>
2016-04-10 02:08:18 -0700

    Minimap is now a circle
    Minimap is now a circle

---
+++ Assets/Scripts/MiniMapUI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MiniMapUI : MonoBehaviour
+{
+
+    public RenderTexture miniMapTexture;
+    public Material miniMapMaterial;
+
+    private float offset;
+
+    void Start()
+    {
+        offset = 10;
+    }
+
+    void OnGUI()
+    {
+        if (Event.current.type == EventType.Repaint)
+        {
+            Graphics.DrawTexture(new Rect(0, 0, 256, 256), miniMapTexture, miniMapMaterial);
+        }
+    }
+}
