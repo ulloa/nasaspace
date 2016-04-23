d5364c3a3454fcfee434fc86964798f50e37d489
Kurtaclan <petersontravis@live.com>
2016-04-23 12:03:23 -0700

    Changes to things
    Changes to things

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,40 +1,32 @@
 ﻿using UnityEngine;
-using System.Collections;
 using System.Linq;
 using UnityEditor;
 
 public class Tile : MonoBehaviour
 {
-    public Color CurColor;
-    void Awake()
+    Vector3 Dimensions { get; set; }
+    Vector2 TileLocation { get; set; }
+
+    public void SetHeightMap(Texture2D heightMap, Vector3 dimensions, Vector2 tileLocation)
     {
-        GetComponent<MeshRenderer>().material.SetColor("_Color", new Color(Random.value, Random.value, Random.value));
-        CurColor = GetComponent<MeshRenderer>().material.GetColor("_Color");
+        Dimensions = dimensions;
+        TileLocation = new Vector2(Mathf.Min(Mathf.Max(0, tileLocation.x), 63), Mathf.Min(Mathf.Max(0, tileLocation.y), 31));
+        var terrain = GetComponent<Terrain>();
+        terrain.terrainData.size = Dimensions;
+        TextureConverterV2.ApplyHeightmap(terrain, heightMap);
     }
 
-    void Update()
-    {
-        if (GetComponent<MeshRenderer>().material.GetColor("_Color") != CurColor)
-            GetComponent<MeshRenderer>().material.SetColor("_Color", CurColor);
-    }
-
-    private Vector3 realSize;
-    public Vector3 RealSize
+    private Vector3 center;
+    public Vector3 Center
     {
         get
         {
-            if (realSize == Vector3.zero)
+            if (center == Vector3.zero)
             {
-                Mesh planeMesh = GetComponent<MeshFilter>().mesh;
-                Bounds bounds = planeMesh.bounds;
-                // size in pixels
-                float boundsX = transform.localScale.x * bounds.size.x;
-                float boundsY = transform.localScale.y * bounds.size.y;
-                float boundsZ = transform.localScale.z * bounds.size.z;
-                realSize = new Vector3(boundsX, boundsY, boundsZ);
+                center = transform.position + new Vector3(Dimensions.x / 2, Dimensions.y, -Dimensions.z / 2);
             }
 
-            return realSize;
+            return center;
         }
     }
 
@@ -42,45 +34,61 @@ public class Tile : MonoBehaviour
     public GameObject NegativeX;
     public GameObject PlusZ;
     public GameObject NegativeZ;
-    public void OnWillRenderObject()
+    public void OnBecameVisible()
     {
         if (!EditorApplication.isPaused)
         {
             Collider collider = null;
             if (PlusX == null)
             {
-                collider = Physics.OverlapBox(transform.GetComponent<Renderer>().bounds.center + new Vector3(RealSize.x, 0, 0), new Vector3(0.1f, 0.1f, 0.1f)).FirstOrDefault();
+                collider = Physics.OverlapBox(Center + new Vector3(Dimensions.x, 0, 0), new Vector3(0.1f, 0.1f, 0.1f)).FirstOrDefault();
                 if (collider != null)
                     PlusX = collider.gameObject;
                 else
-                    PlusX = (GameObject)Instantiate(gameObject, new Vector3(transform.position.x + RealSize.x, transform.position.y, transform.position.z), Quaternion.Euler(0, 0, 0));
+                {
+                    PlusX = (GameObject)Instantiate(gameObject, new Vector3(transform.position.x + Dimensions.x, transform.position.y, transform.position.z), Quaternion.Euler(0, 0, 0));
+                    var tempDimension = new Vector3();
+                    PlusX.GetComponent<Tile>().SetHeightMap(GrabTile.MarsGetTile((short)TileLocation.x, (short)TileLocation.y, Direction.Up, out tempDimension), tempDimension, new Vector2(TileLocation.x, TileLocation.y - 1));
+                }
             }
 
             if (PlusZ == null)
             {
-                collider = Physics.OverlapBox(transform.GetComponent<Renderer>().bounds.center + new Vector3(0, 0, +RealSize.z), new Vector3(0.1f, 0.1f, 0.1f)).FirstOrDefault();
+                collider = Physics.OverlapBox(Center + new Vector3(0, 0, +Dimensions.z), new Vector3(0.1f, 0.1f, 0.1f)).FirstOrDefault();
                 if (collider != null)
                     PlusZ = collider.gameObject;
                 else
-                    PlusZ = (GameObject)Instantiate(gameObject, new Vector3(transform.position.x, transform.position.y, transform.position.z + RealSize.z), Quaternion.Euler(0, 0, 0));
+                {
+                    PlusZ = (GameObject)Instantiate(gameObject, new Vector3(transform.position.x, transform.position.y, transform.position.z + Dimensions.z), Quaternion.Euler(0, 0, 0));
+                    var tempDimension = new Vector3();
+                    PlusZ.GetComponent<Tile>().SetHeightMap(GrabTile.MarsGetTile((short)TileLocation.x, (short)TileLocation.y, Direction.Right, out tempDimension), tempDimension, new Vector2(TileLocation.x + 1, TileLocation.y));
+                }
             }
 
             if (NegativeX == null)
             {
-                collider = Physics.OverlapBox(transform.GetComponent<Renderer>().bounds.center + new Vector3(-RealSize.x, 0, 0), new Vector3(0.1f, 0.1f, 0.1f)).FirstOrDefault();
+                collider = Physics.OverlapBox(Center + new Vector3(-Dimensions.x, 0, 0), new Vector3(0.1f, 0.1f, 0.1f)).FirstOrDefault();
                 if (collider != null)
                     NegativeX = collider.gameObject;
                 else
-                    NegativeX = (GameObject)Instantiate(gameObject, new Vector3(transform.position.x - RealSize.x, transform.position.y, transform.position.z), Quaternion.Euler(0, 0, 0));
+                {
+                    NegativeX = (GameObject)Instantiate(gameObject, new Vector3(transform.position.x - Dimensions.x, transform.position.y, transform.position.z), Quaternion.Euler(0, 0, 0));
+                    var tempDimension = new Vector3();
+                    NegativeX.GetComponent<Tile>().SetHeightMap(GrabTile.MarsGetTile((short)TileLocation.x, (short)TileLocation.y, Direction.Right, out tempDimension), tempDimension, new Vector2(TileLocation.x, TileLocation.y + 1));
+                }
             }
 
             if (NegativeZ == null)
             {
-                collider = Physics.OverlapBox(transform.GetComponent<Renderer>().bounds.center + new Vector3(0, 0, -RealSize.z), new Vector3(0.1f, 0.1f, 0.1f)).FirstOrDefault();
+                collider = Physics.OverlapBox(Center + new Vector3(0, 0, -Dimensions.z), new Vector3(0.1f, 0.1f, 0.1f)).FirstOrDefault();
                 if (collider != null)
                     NegativeZ = collider.gameObject;
                 else
-                    NegativeZ = (GameObject)Instantiate(gameObject, new Vector3(transform.position.x, transform.position.y, transform.position.z - RealSize.z), Quaternion.Euler(0, 0, 0));
+                {
+                    NegativeZ = (GameObject)Instantiate(gameObject, new Vector3(transform.position.x, transform.position.y, transform.position.z - Dimensions.z), Quaternion.Euler(0, 0, 0));
+                    var tempDimension = new Vector3();
+                    NegativeZ.GetComponent<Tile>().SetHeightMap(GrabTile.MarsGetTile((short)TileLocation.x, (short)TileLocation.y, Direction.Right, out tempDimension), tempDimension, new Vector2(TileLocation.x - 1, TileLocation.y));
+                }
             }
         }
     }
